b06b4328cf60dff74dc40dbf3e6b10d70456a16f
Priyanshumg <ginkalpriyanshu@gmail.com>
2024-03-18 22:57:54 +0530

    [Priyanshu] Add: Added method to fetch data from the form

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './login-page.component.scss'
 })
 export class LoginPageComponent {
+  onSubmit(Form: NgForm) {
+    console.log("Form Submitted");
+    console.log(Form.value)
+    console.log(Form.value.email)
+    console.log(Form.value.password)
+  }
 
+  forgotEmail() {
+    console.log("Forgot email clicked");
+  }
+
+  createAccount() {
+    console.log("Create account clicked");
+  }
 }
